10191f661949a0fb01da1f4f17f39c7c6d459f81
Patricia Batjom <patriciabatjom@gmail.com>
2018-10-15 15:31:26 +0200

    fin de quete 5

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -14,10 +14,69 @@ class ItemController extends AbstractController
         $items = $itemManager->selectAll();
         return $this->twig->render('item.html.twig', ['items' => $items]);
     }
+
     public function show(int $id)
     {
         $itemManager = new ItemManager($this->pdo);
         $item = $itemManager->selectOneById($id);
         return $this->twig->render('showItem.html.twig', ['item' => $item]);
+    }
+
+
+    public function add()
+    {
+        $error='';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            if(isset($_POST['title']) && !empty($_POST['title'])){
+                if(preg_match("/[a-zA-Z]+/",$_POST['title']))
+                {
+                    $itemManager = new ItemManager($this->pdo);
+                    $item = new Item();
+                    $item->setTitle($_POST['title']);
+                    $id = $itemManager->insert($item);
+                    header('Location:/item/' . $id);
+                    exit();
+                }else {
+                    $error='caractères invalides';
+                }
+            } else {
+
+                $error='le champ doit être renseigné';
+            }
+        }
+
+        return $this->twig->render('addItem.html.twig',['error' => $error]);
+    }
+
+
+
+    public function edit(int $id): string
+    {
+        $itemManager = new ItemManager($this->pdo);
+        $item = $itemManager->selectOneById($id);
+        $error='';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if(isset($_POST['title']) && !empty($_POST['title'])){
+                $item->setTitle($_POST['title']);
+                $itemManager->update($item);
+                header('Location:/item/' . $id);
+                exit();
+            } else {
+                $error='champ à renseigner';
+            }
+        }
+
+        return $this->twig->render('editItem.html.twig', ['item' => $item], ['error'=>$error]);
+    }
+
+
+
+
+    public function delete(int $id)
+    {
+        $itemManager = new ItemManager($this->pdo);
+        $itemManager->delete($id);
+        header('Location:/');
     }
 }
